7a0bc01528146e94ca0eec0dc6ea423dbf60ddc3
Carlos Alcala <alcala21@gmail.com>
2020-07-05 00:56:15 -0500

    Made branch with multiplication of matrix by number.

--- processor.py
+++ processor.py
@@ -1,10 +1,6 @@
 n1, m1 = [int(x) for x in input().split()]
 A = [[int(x) for x in input().split()] for _ in range(n1)]
-n2, m2 = [int(x) for x in input().split()]
-B = [[int(x) for x in input().split()] for _ in range(n2)]
-if n1 == n2 and m1 == m2:
-    C = [[a + b for a, b in zip(x, y)] for x, y in zip(A, B)]
-    for row in C:
-        print(*row)
-else:
-    print("ERROR")
+constant = int(input())
+B = [[constant*int(x) for x in row] for row in A]
+for row in B:
+    print(*row)
