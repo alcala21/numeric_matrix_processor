02a4cc3e5c60dfa37975421fc8f37be971278fe4
Carlos Alcala <alcala21@gmail.com>
2020-07-10 21:15:38 -0500

    Working version of the matrix inverse, but it's ugly.

--- processor.py
+++ processor.py
@@ -35,12 +35,22 @@ class Matrix:
         else:
             self.error_message()
 
-    def multiply_by_constant(self, constant):
-        self.values = [[constant*x for x in row] for row in self.values]
+    @staticmethod
+    def matrix_by_matrix(values1, nrows1, ncols1, values2, nrows2, ncols2):
+        if ncols1 == nrows2:
+            values = [[sum([row[i] * values2[i][j] for i in range(nrows2)]) for j in range(ncols2)] for row in values1]
+            return values
+        else:
+            return Matrix.error_message()
+
+
+    @staticmethod
+    def multiply_by_constant(values, constant):
+        return [[constant*x for x in row] for row in values]
 
     def diagonal_transpose(self, type_val=1):
         if type_val == 1:
-            self.values = [[self.values[j][i]  for j in range(self.nrows)] for i in range(self.ncols)]
+            self.values = Matrix.transpose(self.values, self.nrows, self.ncols)
         else:
             self.values = [[self.values[j][i]  for j in range(self.nrows)[::-1]] for i in range(self.ncols)[::-1]]
 
@@ -51,6 +61,10 @@ class Matrix:
             self.values = [row for row in self.values[::-1]]
 
     @staticmethod
+    def transpose(values, nrows, ncols):
+        return [[values[j][i]  for j in range(nrows)] for i in range(ncols)]
+
+    @staticmethod
     def determinant(values, nrows, ncols):
         if nrows == ncols == 1:
             return values[0][0]
@@ -58,15 +72,49 @@ class Matrix:
         if nrows == ncols == 2:
             return values[0][0] * values[1][1] - values[0][1] * values[1][0]
         out_det = 0
-        for i, x in enumerate(values[0]):
+        for i in range(ncols):
             minor = [[item for j, item in enumerate(row) if i != j] for row in values[1:]]
-            out_det += x * ((-1) ** (2 + i)) * Matrix.determinant(minor, nrows - 1, ncols - 1)
+            out_det += values[0][i] * ((-1) ** (2 + i)) * Matrix.determinant(minor, nrows - 1, ncols - 1)
         return out_det
+
+    @staticmethod
+    def cofactors(values, nrows, ncols):
+        cof_matrix = list()
+        for i in range(nrows):
+            local_row = list()
+            for j in range(ncols):
+                minor = [[item for l, item in enumerate(row) if l != j] for k, row in enumerate(values) if k != i]
+                cof = ((-1) ** (2 + i + j)) * Matrix.determinant(minor, nrows - 1, ncols - 1)
+                local_row.append(cof)
+            cof_matrix.append(local_row)
+        return cof_matrix
+
+    @staticmethod
+    def inverse(values, nrows, ncols):
+        cof_matrix = Matrix.cofactors(values, nrows, ncols)
+        det_value = Matrix.determinant(values, nrows, ncols)
+        if det_value == 0:
+            print("This matrix doesn't have an inverse.")
+        else:
+            inv_matrix = Matrix.multiply_by_constant(
+                Matrix.transpose(cof_matrix, nrows, ncols), 1 / det_value)
+        return inv_matrix
 
 
     def print_determinant(self):
         print("The result is:")
         print(self.determinant(self.values, self.nrows, self.ncols))
+
+
+    def print_inverse(self):
+        print("The result is:")
+        inv_matrix = self.inverse(self.values, self.nrows, self.ncols)
+        for row in inv_matrix:
+            print(*row)
+            # row_text = ""
+            # for x in row:
+            #     row_text += f"{round(x, 2) + 0: .2f} "
+            # print(row_text.rstrip())
 
 
 def select_from_menu():
@@ -75,6 +123,7 @@ def select_from_menu():
     print("3. Multiply matrices")
     print("4. Transpose matrix")
     print("5. Calculate a determinant")
+    print("6. Inverse matrix")
     print("0. Exit")
     return int(input("Your choice: "))
 
@@ -112,7 +161,7 @@ def input_matrices():
 def matrix_by_constant():
     matrix = Matrix("Enter size of matrix", "matrix")
     constant = input_constant()
-    matrix.multiply_by_constant(constant)
+    matrix.values = Matrix.multiply_by_constant(matrix.values, constant)
     matrix.print()
 
 
@@ -132,9 +181,14 @@ def calculate_determinant():
     matrix.print_determinant()
 
 
+def inverse_matrix():
+    matrix = Matrix("Enter matrix size", "matrix")
+    matrix.print_inverse()
+
+
 def perform_operation(menu_option):
     operations = [add_matrices, matrix_by_constant, multiply_matrices,
-                    transpose_matrix, calculate_determinant]
+                    transpose_matrix, calculate_determinant, inverse_matrix]
     if menu_option in range(len(operations) + 1):
         operations[menu_option - 1]()
     print("\n")
